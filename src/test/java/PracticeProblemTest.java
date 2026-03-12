8644df4644d767a78c57f18ae9be5ab08449462c
ALEX-L1ANG <343293684@tdsb.ca>
2026-03-12 14:25:47 +0000

    update1

--- src/test/java/PracticeProblemTest.java
+++ src/test/java/PracticeProblemTest.java
@@ -4,8 +4,24 @@ import org.junit.jupiter.api.*;
 public class PracticeProblemTest {
 
    @Test
-   void testExample() {
+   void circleAreaTest1() {
       assertEquals(0, PracticeProblem.circleArea(0));
+   }
+   @Test
+   void circleAreaTest2(){
+      assertEquals(3.14, Math.PI,PracticeProblem.circleArea(1));
+   }
+   @Test 
+   void circleAreaTest3(){
+      assertEquals(Math.PI * 100 * 100, PracticeProblem.circleArea(100));
+   }
+   @Test
+   void circleAreaTest4(){
+      assertEquals(Math.PI * 2.5 * 2.5, PracticeProblem.circleArea(2.5));
+   }
+   @Test
+   void circleAreaTest5(){
+      assertEquals(Math.PI * 2 * 2, PracticeProblem.circleArea(2));
    }
 
 }
